86e847d4a3ff07b2000e54e79b2101eeec62acb8
cordovabarbara <aliendocordova@gmail.com>
2025-10-06 10:25:49 -0500

    Agregar CI/CD con pytest, Selenium y reportes HTML"

--- conftest.py
+++ conftest.py
@@ -1,16 +1,19 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
-#URL
+import pytest
 
 BASE_URL = "https://parabank.parasoft.com/parabank/index.htm"
 
 @pytest.fixture()
 def browser():
-    #inicia y cierra el navegador
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
     yield driver
     driver.quit()
