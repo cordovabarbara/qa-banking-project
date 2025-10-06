447a772587d7419b877b11c7939537f27e59d496
cordovabarbara <aliendocordova@gmail.com>
2025-10-06 08:58:27 -0500

    feat: agregar página de transferencia con localizadores y métodos de envío de fondos

---
+++ pages/transfer_page.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TransferPage:
+    #Localizadores
+    TRANSFER_FUNDS_LINK = (By.XPATH,"//a[@href='transfer.htm']")
+    AMOUNT_INPUT = (By.ID,"amount")
+    FROM_ACCOUNT_SELECT = (By.ID,"fromAccountId")
+    TO_ACCOUNT_SELECT = (By.ID,"toAccountId")
+    TRANSFER_BUTTON = (By.XPATH,"//input[@type='submit' and @value='Transfer']")
+    SUCCESS_MESSAGE = (By.XPATH,"//h1[contains(text(),'Transfer Complete')]")
+    ERROR_MESSAGE = (By.XPATH,"//h1[@class='title' and contains(text(), 'Error')]")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def open_transfer_page(self):
+        self.wait.until(EC.element_to_be_clickable(self.TRANSFER_FUNDS_LINK)).click()
+
+    def transfer(self, amount, from_account=None, to_account=None):
+        self.driver.find_element(*self.AMOUNT_INPUT).send_keys(amount)
+        if from_account:
+            from_select = Select(self.driver.find_element(*self.FROM_ACCOUNT_SELECT))
+            from_select.select_by_value(from_account)
+        if to_account:
+            to_select = Select(self.driver.find_element(*self.TO_ACCOUNT_SELECT))
+            to_select.select_by_value(to_account)
+
+        self.driver.find_element(*self.TRANSFER_BUTTON).click()
+
+    def get_success_message(self):
+        return self.wait.until(EC.visibility_of_element_located(self.SUCCESS_MESSAGE)).text
+
+    def get_error_message(self):
+            return self.driver.find_element(*self.ERROR_MESSAGE).text
